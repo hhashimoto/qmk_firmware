941da22ec057adaaefe8a1543b802cdd242df2c6
hashimoto <hashimoto@jupit.co.jp>
2018-08-21 21:08:39 +0900

    make失敗するため、コメントを除去

--- keyboards/mint60/keymaps/h2/keymap.c
+++ keyboards/mint60/keymaps/h2/keymap.c
@@ -73,13 +73,6 @@ enum custom_keycodes {
  *   \  X  C  V  ,       Y  H  J  L  .
  */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//  [0] = LAYOUT( \
-//    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,  \
-//    KC_TAB,    KC_Q,    KC_W,    KC_COMM, KC_DOT,  KC_SCLN,   KC_M,    KC_R,    KC_D,    KC_Y,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
-//    KC_CAPS,     KC_A,    KC_O,    KC_E,    KC_I,    KC_U,      KC_G,    KC_T,    KC_K,    KC_S,    KC_N,    KC_QUOT,        KC_ENT,   \
-//    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_F,      KC_B,    KC_H,    KC_J,    KC_L,    KC_SLSH, KC_RSFT,   KC_UP,  MO(1),   \
-//    KC_ESC,    KC_LCTL,    KC_LGUI,    KC_LALT,    KC_SPC,        KC_BSPC, KC_ENT,  LALT(KC_GRV),               KC_LEFT,KC_DOWN,KC_RGHT \
-//  ),
   [0] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,  \
     KC_TAB,    KC_Q,    KC_W,    KC_U,    KC_S,    KC_G,   KC_SLASH,    KC_M,    KC_D,    KC_N,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
