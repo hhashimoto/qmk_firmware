dbe0e862b5b1f4780d05f9ea85317d0244e7e704
hashimoto <hashimoto@jupit.co.jp>
2018-08-21 21:21:23 +0900

    近い場所にDEL無しは若干不便だったため、Ctrlを潰してDELを配置

--- keyboards/mint60/keymaps/h2/keymap.c
+++ keyboards/mint60/keymaps/h2/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_U,    KC_S,    KC_G,   KC_SLASH,    KC_M,    KC_D,    KC_N,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
     KC_LCTL,     KC_A,    KC_E,    KC_I,    KC_O,    KC_Z,      KC_B,    KC_F,    KC_K,    KC_R,    KC_T,    KC_QUOT,        KC_ENT,   \
     KC_LSFT,       KC_BSLASH,    KC_X,    KC_C,    KC_V,    KC_COMM,      KC_Y,    KC_H,    KC_J,    KC_L,    KC_DOT, KC_SCOLON,   KC_UP,  MO(1),   \
-    KC_ESC,    KC_LCTL,    KC_LGUI,    KC_LALT,    KC_SPC,        KC_RSFT, KC_ENT,  LALT(KC_GRV),               KC_LEFT,KC_DOWN,KC_RGHT \
+    KC_ESC,    KC_BSPACE,    KC_LGUI,    KC_LALT,    KC_SPC,        KC_RSFT, KC_ENT,  LALT(KC_GRV),               KC_LEFT,KC_DOWN,KC_RGHT \
   ),
   [1] = LAYOUT( \
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
